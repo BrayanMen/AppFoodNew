073eff0d688f67daa0d65a03d05fb247396b647e
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2023-10-20 00:13:25 +0000

    reviews

--- public/src/Components/Cards/RecipeRate.jsx
+++ public/src/Components/Cards/RecipeRate.jsx
@@ -1,8 +1,12 @@
 import Titles from '../Titles'
 import { BsBookmarkStarFill } from 'react-icons/bs'
+import { Message, Select } from '../UsedInputs'
+import { useState } from 'react'
+import Rating from '../Stars'
 
 function RecipeRate({ recipe }) {
-    const Rating = [
+    const [rating, setRating] = useState()
+    const Ratings = [
         {
             title: "0 - Inedible",
             value: 0
@@ -31,16 +35,27 @@ function RecipeRate({ recipe }) {
     return (
         <div className='my-12'>
             <Titles title='Reviews' Icon={BsBookmarkStarFill} />
-            <div className='mt-10 xl:grid flex-colo grid-cols-5 gap-12 bg-blueP xs:p-10 py-10 px-2 sm:p-20 rounded'>
+            <div className='mt-10 xl:grid flex-colo grid-cols-5 gap-12 bg-gray-500 xs:p-10 py-10 px-2 sm:p-20 rounded'>
                 <div className='xl:col-span-2 w-full flex flex-col gap-8'>
                     <h3 className='text-xl text-black font-semibold'>
                         Review "{recipe?.name}"
                     </h3>
-                    <p className='text-sm leading-7 font-medium text-gray-800  '> 
-                    Write a review for this recipe.                      
+                    <p className='text-sm leading-7 font-medium text-gray-800  '>
+                        Write a review for this recipe.
                     </p>
-                    <div className='text-sm w-full'>                        
+                    <div className='text-sm w-full'>
+                        <Select
+                            label='Select Rating'
+                            options={Ratings}
+                            onChange={(e) => setRating(e.target.value)} />
+                        <div className='text-yellow-500 mt-4 flex gap-2 text-lg'>
+                            <Rating value={rating} />
+                        </div>
                     </div>
+                    <Message label={'Message'} placeholder={'Write about the recipe...'}/>
+                    <button className='bg-secondary font-semibold hover:bg-red-700 text-white py-3 hover:text-white flex-colo rounded'>
+                        Submit
+                    </button>
                 </div>
             </div>
         </div>
--- public/src/Components/UsedInputs.jsx
+++ public/src/Components/UsedInputs.jsx
@@ -1,22 +1,27 @@
-export const Message = ({label, placeholder}) =>{
-    return(
+export const Message = ({ label, placeholder }) => {
+    return (
         <div className="text-sm w-full">
             <label className="text-gray-800 font-semibold">{label}</label>
-            <textarea className="w-full h-40 mt-2 p-6 border border-greenP rounded"
-            placeholder={placeholder}></textarea>
+            <textarea className="w-full h-40 mt-2 p-6 border border-primary text-white bg-greenP rounded"
+                placeholder={placeholder}></textarea>
         </div>
     )
 }
 
-export const Select = ({label, options, onChange}) => {
-    return(
+export const Select = ({ label, options, onChange }) => {
+    return (
         <div>
-            <label className="">{label}</label>
+            <label className="font-bold ">{label}</label>
             <select onChange={onChange} className='relative border border-primary w-full text-white bg-greenP rounded-lg cursor-default py-3 pl-6 pr-10 text-left text-xs'>
-                    <option className='absolute z-10 mt-1 max-h-60 w-full  text-white ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm'>
-                        {options}
-                    </option>             
-                </select>
+                {options.map((option, i) => (
+                    (<option
+                        key={i}
+                        value={option.value}
+                        className='absolute mt-1 max-h-60 w-full  text-white ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm'>
+                        {option.title}
+                    </option>)
+                ))}
+            </select>
         </div>
     )
 }
